3084f8c21aa1baedc31e6cf6098f9e788a989514
John Zhao <john.zhao@mwhc.com>
2021-07-22 08:11:17 -0700

    added CT date constraint

---
+++ ESAPIX_StarterUI1/ViewModels/CTDateConstraint.cs
@@ -0,0 +1,40 @@
+﻿using ESAPIX.Constraints;
+using ESAPIX.Extensions;
+using System;
+using VMS.TPS.Common.Model.API;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class CTDateConstraint : IConstraint
+    {
+        public string Name => "CT Date Checker";
+        public string FullName => "CT Date < 60 Days";
+
+        public ConstraintResult CanConstrain(PlanningItem pi)
+        {
+            var pq = new PQAsserter(pi); //Plan Quality asserter is a helper function for the CanConstrain method
+            return pq.HasImage().CumulativeResult; //HasImage() runs through a full list of properties that it checks and then attribute .CumulativeResult displays the combined result
+        }
+
+        public ConstraintResult Constrain(PlanningItem pi)
+        {
+            var image = pi.GetImage(); //equivalent ESAPI function is pi.StructureSet.Image, extra ESAPIX methods will appear as an icon with a small arrow next to it
+            return Constrain(image.CreationDateTime); //take the image and pass it into a different method to evaluate CreationDateTime in order to make this step testable
+        }
+
+        public ConstraintResult Constrain(DateTime? creationDate)
+        {
+            var diffDays = (DateTime.Now - creationDate).Value.TotalDays;
+            var msg = $"CT is {diffDays} days old";
+
+            if (diffDays <= 60)
+            {
+                return new ConstraintResult(this, ResultType.PASSED, msg);
+            }
+            else
+            {
+                return new ConstraintResult(this, ResultType.ACTION_LEVEL_3, msg);
+            }
+        }
+    }
+}
--- ESAPIX_StarterUI1/ViewModels/MainViewModel.cs
+++ ESAPIX_StarterUI1/ViewModels/MainViewModel.cs
@@ -21,19 +21,19 @@ namespace ESAPX_StarterUI.ViewModels
 
         public MainViewModel()
         {
-            EvaluateCommand = new DelegateCommand(() =>
+            EvaluateCommand = new DelegateCommand(() => //defines what the button click does
             {
                 foreach (var pc in Constraints)
                 {
-                    var result = VMS.GetValue(sc =>
+                    var result = VMS.GetValue(sc => //when calling this object, it does it on a background thread (frees up the primary processing thread)
                     {
-                        //Check if we can constrain first
+                        //Check if we can constrain first, method CanConstrain checks to see if applying a constraint is valid
                         var canConstrain = pc.Constraint.CanConstrain(sc.PlanSetup);
                         //If not..report why
                         if (!canConstrain.IsSuccess) { return canConstrain; }
                         else
                         {
-                            //Can constrain - so do it
+                            //Can constrain - so do it, method Constrain applies the rule
                             return pc.Constraint.Constrain(sc.PlanSetup);
                         }
                     });
@@ -42,23 +42,24 @@ namespace ESAPX_StarterUI.ViewModels
                 }
             });
 
-            CreateConstraints();
+            CreateConstraints(); //method CreateConstraints creates the constraints...
         }
 
         private void CreateConstraints()
         {
-            Constraints.AddRange(new PlanConstraint[] //plan constraint class will hold the actual constraint AND the result
+            Constraints.AddRange(new PlanConstraint[]
+            //plan constraint class will hold the actual constraint AND the result, change the strings here in order to change the constraints for different treatment sites
             {
                 new PlanConstraint(ConstraintBuilder.Build("PTV", "Max[%] <= 110")),
                 new PlanConstraint(ConstraintBuilder.Build("Rectum", "V75Gy[%] <= 15")),
                 new PlanConstraint(ConstraintBuilder.Build("Rectum", "V65Gy[%] <= 35")),
                 new PlanConstraint(ConstraintBuilder.Build("Bladder", "V80Gy[%] <= 15")),
-                // new PlanConstraint(new CTDateConstraint())
+                new PlanConstraint(new CTDateConstraint())
             });
         }
 
 
-        public DelegateCommand EvaluateCommand { get; set; }
-        public ObservableCollection<PlanConstraint> Constraints { get; private set; } = new ObservableCollection<PlanConstraint>();
+        public DelegateCommand EvaluateCommand { get; set; } //this is the button relay
+        public ObservableCollection<PlanConstraint> Constraints { get; private set; } = new ObservableCollection<PlanConstraint>(); //this is the list of constraints
     }
 }
