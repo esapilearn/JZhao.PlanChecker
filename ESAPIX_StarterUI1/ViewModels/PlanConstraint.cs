4d9872f627c59a45f01155420b449664a34b691c
John Zhao <john.zhao@mwhc.com>
2021-07-22 07:49:28 -0700

    created viewmodel, added constraint, light green button

---
+++ ESAPIX_StarterUI1/ViewModels/PlanConstraint.cs
@@ -0,0 +1,30 @@
+﻿using ESAPIX.Constraints;
+using Prism.Mvvm;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class PlanConstraint : BindableBase //we use BindableBase to access SetProperty, it says when this SetProperty(value) changes, then I need to update the UI
+    {
+        public PlanConstraint(IConstraint con)
+        {
+            this.Constraint = con;
+        }
+
+
+        private IConstraint _constraint;
+
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }
+        }
+
+        private ConstraintResult _result;
+
+        public ConstraintResult Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+    }
+}
